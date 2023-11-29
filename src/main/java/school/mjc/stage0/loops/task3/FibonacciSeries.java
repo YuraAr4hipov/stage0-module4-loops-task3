e3337c0aa260054031c4daf090f9cef3c425d766
Yura Arkhipov <141731482+YuraAr4hipov@users.noreply.github.com>
2023-11-29 15:28:06 +0300

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= lastFibonacci; ++i) {
+            int next = a + b;
+            a = b;
+            b = next;
+            System.out.println(a);
+        }
+        System.out.println(b);
     }
 }
